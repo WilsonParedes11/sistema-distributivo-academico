061ba2b0cf07aec323cc57fff22ba4986029ae7b
WilsonParedes11 <wilsonparedes87@gmail.com>
2025-07-26 01:26:00 -0500

    feat: Creacion del recurso CampusResource para la correcta gestion de recursos

---
+++ app/Filament/Resources/CampusResource.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CampusResource\Pages;
+use App\Filament\Resources\CampusResource\RelationManagers;
+use App\Models\Campus;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CampusResource extends Resource
+{
+    protected static ?string $model = Campus::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-building-library';
+
+    protected static ?string $pluralLabel = 'Campus';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nombre')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('codigo')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('direccion')
+                    ->maxLength(255)
+                    ->default(null),
+                Forms\Components\TextInput::make('telefono')
+                    ->tel()
+                    ->maxLength(255)
+                    ->default(null),
+                Forms\Components\Toggle::make('activo')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nombre')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('codigo')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('direccion')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('telefono')
+                    ->searchable(),
+                Tables\Columns\IconColumn::make('activo')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCampuses::route('/'),
+            'create' => Pages\CreateCampus::route('/create'),
+            'edit' => Pages\EditCampus::route('/{record}/edit'),
+        ];
+    }
+}
